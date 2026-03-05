e54a96bf5c2ab80ef9478d6dfcc363f0cb0ef14c
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-03-05 20:40:55 +0400

    test6

--- py_h2/test6.py
+++ py_h2/test6.py
@@ -1,16 +1,20 @@
-def count_evens(numbers_list):
-    count = 0
-    for n in numbers_list:
-        if n % 2 == 0:
-            count += 1
-            
-            
-    return count
-        
+inventory = [
+    {"product": "iPhone", "price": 1200},
+    {"product": "Samsung", "price": 900},
+    {"product": "MacBook", "price": 2500},
+    {"product": "Xiaomi", "price": 400}
+]
 
-my_list = [1, 2, 3, 4, 5, 6]
-result = count_evens(my_list)
-print(f"In list is {result} number")
+max_price = 0
+
+for item in inventory:
+    if item["price"] > max_price:
+        max_price = item["price"]
+
+
+print(max_price)
+
+
 
          
         
