c9ea738afc517d2d92b0e42ab49ac87c3df14e86
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-03-09 23:40:37 +0400

    execises

--- py_h2/execises.py
+++ py_h2/execises.py
@@ -1,6 +1,23 @@
-fruits = ["apple", "banana", "kiwi"]
-fruits.append("orange")
-for fruit in fruits:
-    print(fruit)
+prices = [10, 20, 30]
+
+def add_vat(price):
+    result = price * 1.18
+    return round(result, 2)
+
+def process_prices(price_list):
+    new_list = []
+    for p in price_list:
+       result = add_vat(p) 
+       new_list.append(result)
+    return new_list
+
+final_prices = process_prices(prices)
+
+print(final_prices)
+
+
+  
+
+
 
 
