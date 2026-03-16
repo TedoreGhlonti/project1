67e7bfd346e36f7d37472161c583e42c7e8bdc5c
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-03-16 22:50:14 +0400

    functions

--- python_home/functions.py
+++ python_home/functions.py
@@ -1,29 +1,14 @@
-def get_valid_age():
-    while True:
-        user_input = input("გთხოვთ შეიყვანოთ თქვენი ასაკი: ")
-        
-        if user_input.isdigit():
-            age = int(user_input)
-            if 0 <= age <= 120:
-                return age  
-            else:
-                print("შეცდომა: ასაკი უნდა იყოს 0-დან 120-მდე!")
-        else:
-            print("შეცდომა: გთხოვთ შეიყვანოთ მხოლოდ ციფრები!")
+# 1. პატარა ფუნქცია: უმატებს 5-ს
+def add_five(n):
+    return n + 5
 
-def calculate_price(age):
-    if age < 18:
-        return "ფასი: 10 ლარი"
-    elif age >= 65:
-        return "ფასი: 5 ლარი"
-    else:
-        return "ფასი: 20 ლარი"
+# 2. მთავარი ფუნქცია: აორმაგებს იმას, რასაც მიიღებს
+def double_number(number):
+    # აქ ვიძახებთ პირველ ფუნქციას! 
+    # ჯერ ემატება 5, მერე მრავლდება 2-ზე
+    result = add_five(number) * 2
+    return result
 
-my_age = get_valid_age()
-
-final_price = calculate_price(my_age)
-
-print(f"თქვენი ასაკია {my_age}, შესაბამისად {final_price}")
-
-
-
+# გამოძახება
+final_val = double_number(10)
+print(final_val) # დაბეჭდავს 30-ს (რადგან (10+5) * 2 = 30)
