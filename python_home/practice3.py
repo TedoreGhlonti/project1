e2029622f03bfbca3c4b3e99cf363ede4b273137
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-03-30 21:10:46 +0400

    practice3

--- python_home/practice3.py
+++ python_home/practice3.py
@@ -1,10 +1,21 @@
-while True:
+correct_pin = 211  
+attempts = 0       
+
+while attempts < 3:
     try:
-        x = int(input("What's x? "))
+        user_pin = int(input("Enter PIN: "))
+
+        if user_pin == correct_pin:
+            print("Access Granted! 💰")
+            break
+        else:
+            attempts += 1
+            remaining = 3 - attempts
+            print(f"Wrong PIN! You have {remaining} attempts left.")
+
     except ValueError:
-        print("x is not an integer")
-    else:
-        break
-    
-    
-print(f"x is {x}")
+        attempts += 1
+        remaining = 3 - attempts
+        print(f"Invalid input! Use numbers only. {remaining} attempts left.")
+if attempts == 3:
+    print("Card Blocked! ❌ Please contact your bank.")
