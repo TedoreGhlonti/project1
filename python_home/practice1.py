242dc1193f75d3c4d9f161f862b6412ea21bfc3b
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-04-06 12:56:07 +0400

    nike

--- python_home/practice1.py
+++ python_home/practice1.py
@@ -1,6 +1,12 @@
+def divide_numbers(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return "შეცდომა: ნულზე გაყოფა არ შეიძლება!"
+    except TypeError:
+        return "შეცდომა: გთხოვთ მიაწოდოთ მხოლოდ რიცხვები და არა ტექსტი!"
 
-
-
+print(divide_numbers(15, 5)) # ახლა ესეც აღარ "გააფუჭებს" პროგრამას
     
 
 
