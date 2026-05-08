8fea682e1ae1dfb4480f02c816d5060bf17ca4b8
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-05-08 23:58:55 +0400

    class

--- class.py
+++ class.py
@@ -1,12 +1,31 @@
-class Car:
-
-    def __init__(self, brand, year):
+class Smartphone:
+    def __init__(self, brand, battery):
         self.brand = brand
-        self.year = year
+        self.battery = battery
 
-    def show_info(self):
+    def play_game(self):
+        if self.battery >= 20:
+            self.battery -= 20
+            return "Playing game... 🎮"
+        else:
+            return "Battery too low! 🪫"
 
-        return f"Brand: {self.brand} | Year: {self.year}"
-    
-result = Car("BMW", 2022)
-print(result.show_info())
+    def charge(self):
+        self.battery += 10
+        if self.battery > 100: self.battery = 100 # ლიმიტი 100%
+        return "Charging... ⚡"
+
+    def check_status(self):
+        return f"Brand: {self.brand} | Battery: {self.battery}%"
+
+# ტესტირება
+phone = Smartphone("Honor", 87)
+
+print(phone.play_game())     # დაიბეჭდება: Playing game...
+print(phone.check_status())  # დაიბეჭდება: Brand: Honor | Battery: 67%
+print(phone.charge())        # დაიბეჭდება: Charging...
+print(phone.check_status())  # დაიბეჭდება: Brand: Honor | Battery: 77%
+
+        
+        
+        
