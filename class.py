0f6fe43ac0799309a6f5355d857b3e459c332519
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-05-11 22:34:43 +0400

    class

--- class.py
+++ class.py
@@ -1,25 +1,32 @@
-class Robot:
-    def action(self):
+class Accaunt():
+
+    def __init__(self, balance):
+
+        self.balance = balance
+
+    def deposit(self, amount):
+
+        self.balance += amount
+    
+    def withdraw(self, amount):
+
+        self.balance -= amount
+    
+    def display(self):
+
+        return f"Current balance: {self.balance}"
+    
+result = Accaunt(500)
+result.deposit(200)
+result.withdraw(100)
+print(result.display())
+
         
-        return "I am a robot, I help people."
+        
+        
+        
+        
     
-class CleanerRobot(Robot):
-    def action(self):
-        return "I am cleaning the floor."
-    
-class CookRobot(Robot):
-
-    def action(self):
-        return "I am cooking dinner."
-    
-
-basic_robot = Robot()
-cool_robot = CleanerRobot()
-other_robot = CookRobot()
-
-print(basic_robot.action())
-print(cool_robot.action())
-print(other_robot.action())
         
 
 
