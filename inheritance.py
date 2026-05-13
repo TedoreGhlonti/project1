da1482488a9f71dedfbe7a08dbe3817fa02b9a17
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-05-13 20:16:33 +0400

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,37 +1,32 @@
-class Car:
+class Fruit:
+    fruits_list = []
 
-    def __init__(self, brand, year, color):
-        self.brand = brand
-        self.year = year
-        self.color = color
+    def __init__(self, item, price, quantity):
+        self.item = item
+        self.price = price
+        self.quantity = quantity
+        
+        Fruit.fruits_list.append(self)
 
-class Lightcars(Car):
+class List(Fruit):
+    def info(self):
+        total = self.price * self.quantity
+        return f"Item: {self.item:<7} | Total Value: {total:>6} GEL"
 
-    def cars_info(self):
 
-        return f"Car: {self.brand}, Year: {self.year}, Color: {self.color}"
-    
-class Trucks(Car):
+fr1 = List("Apple", 2.5, 30)
+fr2 = List("Banana", 3.5, 35)
+fr3 = List("Kiwi", 2.8, 45)
 
-    def cars_info(self):
+print("--- Fruits Detailed List ---")
+for f in Fruit.fruits_list:
+    print(f.info())
+        
+        
 
-        return f"Truck: {self.brand}, Year: {self.year}, Color: {self.color}"
-    
-car1 = Lightcars("Mercedes", 2022, "black")
-car2 = Lightcars("BMW", 2025, "red")
-truck1 = Trucks("Scania", 2023, "orange")
-truck2 = Trucks("DAF", 2024, "white")
 
-car_list = [car1, car2, truck1, truck2]
-
-print("--- CAR LIST ---")
-print("-" * 20)
-
-for c in car_list:
-    print(c.cars_info())
-
-print("-" * 20)
-
+        
+        
         
 
 
